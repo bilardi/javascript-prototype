3bbb3cf6ee6d6fc228cf2348411dedcd8e419074
Alessandra Bilardi <alessandra.bilardi@pixartprinting.com>
2017-11-15 22:14:04 +0100

    step 3 - add the first function of my Helper script: the SpecRunner.html works properly

---
+++ src/MyHelper.js
@@ -0,0 +1,23 @@
+/**
+ * Helper of an example
+ *
+ * An example of helper that it contains some functions manage boolean variables.
+ *
+ * @license MIT
+ * @author Alessandra Bilardi <alessandra.bilardi@gmail.com>
+ * @see https://github.com/bilardi/javascript-prototype for details
+ */
+
+/**
+ * bar variable
+ * @type {boolean}
+ */
+var bar = true;
+
+/**
+ * Set bar variable
+ * @param {boolean} bar
+ */
+function setBar(bool = true) {
+    bar = Boolean(bool);
+}
